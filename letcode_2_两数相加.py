d1ca2a123fcf338d9c332e438d4247f0660b307d
lihong <313071162@qq.com>
2020-10-04 09:29:46 +0800

    letcode_2

--- letcode_2_两数相加.py
+++ letcode_2_两数相加.py
@@ -17,10 +17,9 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
+class Solutions:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         advance = 0
-        remender = 0
         l3 = ListNode(-1)
         # 此处需要加一个开始结点来记录l3最初的位置，否则l3只会指向其当前结点和他后面的那个结点
         start = l3
@@ -41,3 +40,42 @@ class Solution:
         start = start.next
         return start
 
+
+class Solution(object):
+    """
+    这也是二刷这道题，刚做的时候忘了考虑进位的问题
+    """
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        advance = 0
+        l3 = ListNode(-1)
+        start = l3
+        while (l1 or l2):
+            sum = int((l1.val if l1 else 0)) + int((l2.val if l2 else 0)) + advance
+            remender = sum % 10
+            l3.next = ListNode(remender)
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+            advance = sum // 10
+            l3 = l3.next
+            if advance == 1:
+                l3.next = ListNode(advance)
+        return start.next
+
+if __name__ == '__main__':
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
+
+    sol = Solution()
+    sol.addTwoNumbers(l1, l2)
